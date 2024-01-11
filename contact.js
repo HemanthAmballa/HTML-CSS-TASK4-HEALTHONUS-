9100d50cfc601a19c44dba2f86edf7f35b97447e
HemanthAmballa <152526139+HemanthAmballa@users.noreply.github.com>
2024-01-11 12:36:32 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,101 @@
+function selectValidation(arg){
+    const stateValue =  arg.value;
+    const code = document.getElementById("code");
+    code.value = stateValue+" -"+" PROMO";
+    
+}
+
+function validateGender(arg){
+    
+      if((arg.checked) && (arg.value=="Male")){
+              console.log("hello")
+               alert('“Hello Sir”')        
+      }else if((arg.checked) && (arg.value=="Female")){
+
+                alert('“Hello Lady”')   
+      }
+
+
+}
+
+function  validateEmail(arg){
+      
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isValid = emailPattern.test(arg.value);  
+
+    if(isValid && arg.value!="" ){
+        var spanval =   document.getElementById("spTwo");
+        spanval.innerHTML="*";
+        spanval.className="inner_spanm";
+    }
+}
+
+function validate(){
+    
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var org = document.getElementById("Organization").value;
+    if(name==""){
+        var spanval = document.getElementById("spOne");
+        spanval.innerHTML="Name is Required";
+        spanval.className="spanval"
+    }
+    if(email==""){
+        var spanval =   document.getElementById("spTwo");
+        spanval.innerHTML="Email is Required";
+        spanval.className="spanvaltwo"
+    }
+    if(org==""){
+        var spanval =   document.getElementById("spThree");
+        spanval.innerHTML="Org name is Required";
+        spanval.className="spanvalthree"
+       
+    }
+    console.log("val")
+    event.preventDefault();
+    if(name=="" || email=="" || org=="" ){
+        console.log("val2")
+          document.getElementById("spanone").innerHTML="Please fill all the required fields below";
+     }else{
+        console.log(val3)
+        document.getElementById("spanone").style.display="none";
+     }
+ 
+    
+}
+
+
+function validateName(arg){
+
+    var name = arg.value;
+    if(name!=""){
+        var spanval =   document.getElementById("spOne");
+        spanval.innerHTML="*";
+        spanval.className="inner_span";
+     }
+}
+
+function validateOrg(arg){
+    var org = arg.value;
+    if(org!=""){
+        var spanval =   document.getElementById("spThree");
+        spanval.innerHTML="*";
+        spanval.className="inner_spans";
+    }
+}
+
+function validateReset(){
+  
+    var spanval =   document.getElementById("spOne");
+        spanval.innerHTML="*";
+        spanval.className="inner_span";
+     var spanval2 =   document.getElementById("spTwo");
+        spanval2.innerHTML="*";
+        spanval2.className="inner_spanm";
+    var spanval3 =   document.getElementById("spThree");
+        spanval3.innerHTML="*";
+        spanval3.className="inner_spans";
+      
+        document.getElementById("spanone").innerHTML="";
+
+}
